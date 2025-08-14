89c0c7cdbdbe6d14c14ec8859c94d0c8b086c03e
Italo Rocha <italorochaj@icloud.com>
2025-08-14 14:43:58 -0300

    Implementado VeiculoServico

--- Dominio/Interfaces/IVeiculoServico.cs
+++ Dominio/Interfaces/IVeiculoServico.cs
@@ -6,12 +6,12 @@ namespace minimal_api.Dominio.Interfaces
 {
     public interface IVeiculoServico
     {
-        List<Veiculo>? Todos(int pagina = 1, string? nome = null, string? marca = null);
-        Veiculo BuscaPorId(int id);
+        List<Veiculo> Todos(int? pagina = 1, string? nome = null, string? marca = null);
+        Veiculo? BuscaPorId(int id);
 
-        Veiculo Incluir(Veiculo veiculo);
+        void Incluir(Veiculo veiculo);
 
-        Veiculo Atualizar(Veiculo veiculo);
+        void Atualizar(Veiculo veiculo);
 
         void Apagar(Veiculo veiculo);
 

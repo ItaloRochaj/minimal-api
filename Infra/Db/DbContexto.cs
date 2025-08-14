d1a6661f13286cc638017a2850d30b964ada8992
Italo Rocha <italorochaj@icloud.com>
2025-08-13 23:38:54 -0300

    Criado Migrations Administradores

--- Infra/Db/DbContexto.cs
+++ Infra/Db/DbContexto.cs
@@ -17,7 +17,7 @@ public class DbContexto : DbContext
         if (!optionsBuilder.IsConfigured)
         {
             var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
-            if (string.IsNullOrEmpty(stringConexao))
+            if (!string.IsNullOrEmpty(stringConexao))
             {
                 optionsBuilder.UseMySql(
                     stringConexao,
